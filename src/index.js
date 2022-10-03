24464eef29c67f588a1a1b33066afcaf4db5d605
jorge <jorge@garlicoin.io>
2022-10-03 03:27:11 -0400

    Add redirect

--- src/index.js
+++ src/index.js
@@ -2,5 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import ReactSSLRedirect from "react-ssl-redirect";
 
+ReactSSLRedirect();
 ReactDOM.render(<App />, document.getElementById('root'));
